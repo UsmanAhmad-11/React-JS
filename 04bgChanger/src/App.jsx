9cf5fc5b6af5f557e34e9a396a52f2083b0f80c7
UsmanAhmad-12 <usman123business@gmail.com>
2024-06-15 18:11:51 +0500

    add on's

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -24,6 +24,46 @@ function App() {
         className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
         style={{backgroundColor:"blue"}}
         >Blue</button></div>
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl"><button
+        onClick={()=>setColor("olive")}
+        className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+        style={{backgroundColor:"olive"}}
+        >Olive</button></div>
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl"><button
+        onClick={()=>setColor("gray")}
+        className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+        style={{backgroundColor:"gray"}}
+        >Gray</button></div>
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl"><button
+        onClick={()=>setColor("yellow")}
+        className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+        style={{backgroundColor:"yellow"}}
+        >Yellow</button></div>
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl"><button
+        onClick={()=>setColor("pink")}
+        className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+        style={{backgroundColor:"pink"}}
+        >Pink</button></div>
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl"><button
+        onClick={()=>setColor("purple")}
+        className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+        style={{backgroundColor:"purple"}}
+        >Purple</button></div>
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl"><button
+        onClick={()=>setColor("lavender")}
+        className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+        style={{backgroundColor:"lavender"}}
+        >Lavender</button></div>
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl"><button
+        onClick={()=>setColor("white")}
+        className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+        style={{backgroundColor:"white"}}
+        >White</button></div>
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl"><button
+        onClick={()=>setColor("black")}
+        className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+        style={{backgroundColor:"black"}}
+        >Black</button></div>
       </div>
     </div>
   )
